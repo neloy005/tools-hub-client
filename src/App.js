af9b41af3a92573a995b5b242a61f177a7126fac
neloy005 <arifistiekofficial@gmail.com>
2022-05-22 07:34:26 +0600

    footer done

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import Blogs from './components/Blogs/Blogs';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
       </Routes>
+
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Footer.css';
+import fb from '../../images/icons/facebook.png'
+import ig from '../../images/icons/instagram.png'
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <div className='footer-container'>
+            <h3>Developed by Arif</h3>
+            <p>All rights preserved by &copy;Md. Arif Istiek (Neloy) 2022</p>
+            <div className='footer-link-container'>
+                <Link to='/blog'>Blogs</Link>
+                <Link to='/'>Home</Link>
+            </div>
+            <div>
+                <a href="https://www.facebook.com/profile.php?id=100018011586420" target="_blank"><img src={fb} alt="" /></a>
+                <a href="https://www.instagram.com/arif__istiak/" target="_blank"><img src={ig} alt="" /></a>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
