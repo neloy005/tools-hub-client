bab23fe3296ad10ca41dd6fe6596cc2d9a6c082b
neloy005 <arifistiekofficial@gmail.com>
2022-05-24 05:06:26 +0600

    delete order done

--- src/components/MyOrders/MyOrders.js
+++ src/components/MyOrders/MyOrders.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Table } from 'react-bootstrap';
+import { Button, Table, Modal } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 
 const MyOrders = () => {
     const [user] = useAuthState(auth);
     const [orders, setOrders] = useState([]);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     useEffect(() => {
         if (user) {
             fetch(`http://localhost:5000/order?email=${user.email}`, {
@@ -20,7 +25,22 @@ const MyOrders = () => {
                 });
         }
     }, [user])
-
+    const handleDelete = id => {
+        console.log(id);
+        const url = `http://localhost:5000/order/${id}`;
+        fetch(url, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    toast.success('Deleted successfully!');
+                    const remaining = orders.filter(order => order._id !== id);
+                    setOrders(remaining);
+                }
+            })
+        handleClose();
+    }
     return (
         <div>
             <h3>Hello {user.displayName}, you've {orders.length} orders so far!</h3>
@@ -45,9 +65,25 @@ const MyOrders = () => {
                                 <td>{order.quantity}</td>
                                 <td>${order.toPay}</td>
                                 <td>{
-                                    order.isPaid ? '' : <><Button variant="danger" size="sm">
-                                        Cancel
-                                    </Button><span> Or </span>
+                                    order.isPaid ? '' : <><>
+                                        <Button variant="danger" size="sm" onClick={handleShow}>
+                                            Cancel
+                                        </Button>
+                                        <Modal show={show} onHide={handleClose} animation={false}>
+                                            <Modal.Header closeButton>
+                                                <Modal.Title>Are you sure to cancel the order?</Modal.Title>
+                                            </Modal.Header>
+
+                                            <Modal.Footer>
+                                                <Button variant="secondary" onClick={handleClose}>
+                                                    Close
+                                                </Button>
+                                                <Button variant="danger" onClick={() => handleDelete(order._id)}>
+                                                    Yes
+                                                </Button>
+                                            </Modal.Footer>
+                                        </Modal>
+                                    </><span> Or </span>
                                         <Button variant="success" size="sm">
                                             Pay
                                         </Button></>
@@ -56,6 +92,8 @@ const MyOrders = () => {
                     }
                 </tbody>
             </Table>
+
+
         </div>
     );
 };
