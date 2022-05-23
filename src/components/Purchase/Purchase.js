620ffe571a208e66eb447ac7e9c0e66f291ae698
neloy005 <arifistiekofficial@gmail.com>
2022-05-23 07:15:59 +0600

    single tool data loaded successfully with jwt verification

--- src/components/Purchase/Purchase.js
+++ src/components/Purchase/Purchase.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './Purchase.css';
 
 const Purchase = () => {
     const { id } = useParams();
-
+    const [tool, setTool] = useState({});
+    useEffect(() => {
+        const url = `http://localhost:5000/tool/${id}`;
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                setTool(data);
+            });
+    }, [])
     return (
         <div>
-            <h2>Purchase:{id}</h2>
+            <h2>Purchase:{tool.name}</h2>
         </div>
     );
 };
